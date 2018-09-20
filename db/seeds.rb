fcc080f82bff2b8450d4dc863180826bb70716cc
Masafumi Mochizuki <there_is_a_moon@yahoo.co.jp>
2018-09-20 14:03:42 +0900

    seed made

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# ユーザを作成する関数
+def CreateUser(number)
+  number = number.to_s
+  User.create!(
+    login_name: number,
+    email:   number + "@" + number + ".com",
+    password: number,
+    # 画像はあらかじめアップロードしておく。
+    face: File.open("./app/assets/images/me.gif")
+  )
+end
+
+# ■ユーザの作成
+CreateUser(1111)
+CreateUser(2222)
+CreateUser(3333)
+CreateUser(4444)
+
+# ■meganeの作成
+100.times do |seed|
+  Megane.create!(
+    content: Faker::OnePiece.akuma_no_mi,
+    picture: File.open("./app/assets/images/header_button.png"),
+    user_id: Faker::Number.between(1, 4)
+  )
+end
