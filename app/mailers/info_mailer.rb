13e061a1e8a2a9d324a840f4b9ba6458fccd8a7a
Masafumi Mochizuki <there_is_a_moon@yahoo.co.jp>
2018-09-24 15:37:58 +0900

    mail send_to modified

--- app/mailers/info_mailer.rb
+++ app/mailers/info_mailer.rb
@@ -1,6 +1,6 @@
 class InfoMailer < ApplicationMailer
   def info_mail(info)
     @info = info
-    mail to: "my_email@test.com", subject: "お問合せの確認メール"
+    mail to: info.user.email, subject: "投稿したよん♪"
   end
 end
